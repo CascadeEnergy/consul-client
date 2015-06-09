9fa96087e45f9f512babc6942e1091e72bbd2e0f
Will Vaughn <willieviseoae@gmail.com>
2015-06-09 14:31:20 -0700

    General comments about the service-client.

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ var methods = ['get', 'post'];
  * @param {Object} httpClient with getAsync and postAsync methods
  * @returns {Object} serviceClient with invoke and retrieve methods
  */
+// You mentioned a couple days ago to me that you weren't sure if you liked having
+// to pass the httpClient in here. I definitely think it hurts the ease of use
+// of this module. Not only are you expecting the user to pass in an http client
+// you require it to be a very specific one. It needs to be a Bluebird
+// promisified one, that uses the "Async" suffix. I don't think it is necessary
+// to have the user pass one in, and it should just be internally define. You could
+// either promisify request in here, or use a request library of your choosing
+// which already supports promises.
+
+// This isn't really a discovery "url". I think a url type of string is expected
+// to have a uri scheme like "http://", "https://", "s3://", "file://" etc. I
+// like the name "host" for this variable perhaps.
 module.exports = function(discoveryUrl, httpClient) {
 
   /**
@@ -20,6 +32,10 @@ module.exports = function(discoveryUrl, httpClient) {
    * @param {Object} options can include method, endpoint and data
    * @returns {Promise} resolves with service response
    */
+  // I don't think there's any need to separate serviceName, from the options
+  // hash. The interface could be just (options)
+
+  // options = { serviceName, endpoint, method, body }
    return function(serviceName, options) {
     var defaultOptions = {
       method: 'get'
@@ -27,6 +43,17 @@ module.exports = function(discoveryUrl, httpClient) {
 
     options = _.assign(defaultOptions, options);
 
+    // No need to Bluebird.resolve a promise with options to kick off a promise
+    // chain. You could just validate the options, and throw an error if they're
+    // invalid.
+
+    // validateOptions(options) // if that function hits an invalid it throws.
+    //
+    // Or even this, because you only really validate serviceName
+    //
+    // if (options.serviceName == null) {
+    //   throw new Error('service name required');
+    // }
     return Bluebird.resolve(options)
       .then(validate)
       .then(getServiceUrl)
@@ -40,12 +67,19 @@ module.exports = function(discoveryUrl, httpClient) {
         throw new Error('service name required');
       }
 
+      // Not sure I understand the need for throwing this error.
+      // All HTTP verbs should be acceptable right?
+      // If you try to make a request, and the service you're requesting doesn't
+      // support that HTTP verb, it should fail and come back in the service
+      // response. And that would be enough right?
       if (_.indexOf(methods, options.method) < 0) {
         throw new Error('unsupported method');
       }
 
+      // Does this need to return anything at all?
       return true;
     }
+
 
     function getServiceUrl() {
       return httpClient
@@ -80,10 +114,15 @@ module.exports = function(discoveryUrl, httpClient) {
       }
       request.url = serviceUrl;
 
+      // I'd just call this "body", most things call it body, hapi is the
+      // only thing I know of that calls it payload.
       if (options.payload) {
         request.body = options.payload;
       }
 
+      // This would get a little simpler if you were using your own specific
+      // http client as part of this module, rather than allowing it to be
+      // passed in.
       return httpClient[options.method + 'Async'](request);
     }
 
