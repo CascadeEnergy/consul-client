976e15cd485c13f05f615007c641ca1a193241f3
Will Vaughn <willieviseoae@gmail.com>
2015-06-12 10:12:54 -0700

    Test organization refactor.

--- test/index.js
+++ test/index.js
@@ -8,76 +8,66 @@ describe('service-client', function() {
   var serviceRequest;
   var host = 'my.service.discovery.host.com';
   var hostUrl = 'http://' + host;
+  var serviceName = 'testService';
+  var endpoint = 'testEndpoint';
+  var sampleBody = { beep: 'boop' };
+  var getSetup;
+  var postSetup;
+  var putSetup;
+  var deleteSetup;
 
   beforeEach(function() {
     serviceRequest = serviceClient(host);
   });
 
-  it('should throw an error if no healthy services are found', function(done) {
-    var serviceName = 'testService';
+  // =====================================
+  // Testing requests with HTTP verbs
+  // =====================================
+  getSetup = [
+    'get',
+    undefined,
+    { serviceName: serviceName, endpoint: endpoint }
+  ];
 
-    // Return empty array from service health check call
-    nock(hostUrl)
-      .get('/v1/health/service/' + serviceName + '?passing')
-      .reply(200, []);
+  postSetup = [
+    'post',
+    function(body) { return isEqual(body, sampleBody); },
+    {
+      serviceName: serviceName,
+      endpoint: endpoint,
+      method: 'POST',
+      body: sampleBody
+    }
+  ];
 
-    // Results in an error, "no service instances available"
-    serviceRequest({
-      serviceName: serviceName
-    }).catch(function(err) {
-      assert.equal(err.message, 'no service instances available');
-      done();
-    });
-  });
+  putSetup = [
+    'put',
+    function(body) { return isEqual(body, sampleBody); },
+    {
+      serviceName: serviceName,
+      endpoint: endpoint,
+      method: 'PUT',
+      body: sampleBody
+    }
+  ];
+
+  deleteSetup = [
+    'delete',
+    undefined,
+    { serviceName: serviceName, endpoint: endpoint, method: 'DELETE' }
+  ];
 
   withData({
-    'GET verb': [
-      'get',
-      undefined,
-      { serviceName: 'testService', endpoint: 'testEndpoint' },
-      { foo: 'bar' }
-    ],
-    'POST verb': [
-      'post',
-      function(body) {
-        return isEqual(body, { beep: 'boop' });
-      },
-      {
-        serviceName: 'testService',
-        endpoint: 'testEndpoint',
-        method: 'POST',
-        body: { beep: 'boop' }
-      },
-      { foo: 'bar' }
-    ],
-    'PUT verb': [
-      'put',
-      function(body) {
-        return isEqual(body, { beep: 'boop' });
-      },
-      {
-        serviceName: 'testService',
-        endpoint: 'testEndpoint',
-        method: 'PUT',
-        body: { beep: 'boop' }
-      },
-      { foo: 'bar' }
-    ],
-    'DELETE verb': [
-      'delete',
-      undefined,
-      {
-        serviceName: 'testService',
-        endpoint: 'testEndpoint',
-        method: 'DELETE'
-      },
-      { foo: 'bar' }
-    ]
-  }, function(verb, bodyValidateFn, requestConfig, serviceResponse) {
+    'GET verb': getSetup,
+    'POST verb': postSetup,
+    'PUT verb': putSetup,
+    'DELETE verb': deleteSetup
+  }, function(verb, bodyValidateFn, requestConfig) {
     it('should make service request', function(done) {
       var address = 'test.service.com';
       var port = 4242;
       var serviceUrl = 'http://' + address + ':' + port;
+      var serviceResponse = { foo: 'bar' };
 
       // Health check call responds with one healthy service.
       nock(hostUrl)
@@ -94,6 +84,24 @@ describe('service-client', function() {
           assert.deepEqual(response.body, serviceResponse);
           done();
         });
+    });
+  });
+
+  // =====================================
+  // Testing service instances not found.
+  // =====================================
+  it('should throw an error if no healthy services are found', function(done) {
+    // Return empty array from service health check call
+    nock(hostUrl)
+      .get('/v1/health/service/' + serviceName + '?passing')
+      .reply(200, []);
+
+    // Results in an error, "no service instances available"
+    serviceRequest({
+      serviceName: serviceName
+    }).catch(function(err) {
+      assert.equal(err.message, 'no service instances available');
+      done();
     });
   });
 });
