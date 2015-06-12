a8f7bfdf342b3fa661f305fe18540e2e10b0f699
Will Vaughn <willieviseoae@gmail.com>
2015-06-11 21:40:14 -0700

    a passing test and some missing semicolons.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var nock = require('nock');
 var serviceClient = require('../index');
 
@@ -7,15 +8,25 @@ describe('service-client', function() {
   var serviceRequest;
 
   beforeEach(function() {
-    serviceRequest = serviceClient(host)
+    serviceRequest = serviceClient(host);
   });
 
   it('should throw an error if no healthy services are found', function(done) {
     var serviceName = 'testService';
+
+    // Return empty array from service health check call
     nock('http://' + host)
       .get('/v1/health/service/' + serviceName + '?passing')
-      .reply(200, {body: []});
+      .reply(200, []);
 
-    serviceRequest()
+    // Results in an error, "no service instances available"
+    serviceRequest({
+      serviceName: serviceName
+    }).catch(function(err) {
+      assert.equal(err.message, 'no service instances available');
+      done();
+    });
   });
+
+  it()
 });
