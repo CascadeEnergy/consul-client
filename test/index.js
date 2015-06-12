a785e37332aa1f0b4ecb67bbc92750b1271d95d4
Will Vaughn <willieviseoae@gmail.com>
2015-06-11 22:23:34 -0700

    Tests passing, still wip. But nock is very nice.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var isEqual = require('lodash/lang/isEqual');
 var nock = require('nock');
 var serviceClient = require('../index');
 
@@ -28,5 +29,57 @@ describe('service-client', function() {
     });
   });
 
-  it()
+  it('should make service request', function(done) {
+    var serviceName = 'testService';
+    var address = 'test.service.com';
+    var port = 1234;
+    var endpoint = 'testEndpoint';
+
+    // Health check call responds with one healthy service.
+    nock('http://' + host)
+      .get('/v1/health/service/' + serviceName + '?passing')
+      .reply(200, [{Service: {Address: address, Port: port}}]);
+
+    // Service is then called.
+    nock('http://' + address + ':' + port)
+      .get('/'+ endpoint)
+      .reply(200, {foo: 'bar'});
+
+    serviceRequest({
+      serviceName: serviceName,
+      endpoint: endpoint
+    }).then(function(response) {
+      assert.deepEqual(response.body, {foo: 'bar'});
+      done();
+    });
+  });
+
+  it('should accept POST body as plain javascript object', function(done) {
+    var serviceName = 'testService';
+    var address = 'test.service.com';
+    var port = 1234;
+    var endpoint = 'testEndpoint';
+
+    // Health check call responds with one healthy service.
+    nock('http://' + host)
+      .get('/v1/health/service/' + serviceName + '?passing')
+      .reply(200, [{Service: {Address: address, Port: port}}]);
+
+    // Service is then called.
+    nock('http://' + address + ':' + port)
+      .post('/'+ endpoint, function(body) {
+        return isEqual(body, { beep: 'boop' });
+      })
+      .reply(200, {foo: 'bar'});
+
+    serviceRequest({
+      serviceName: serviceName,
+      endpoint: endpoint,
+      method: 'POST',
+      body: { beep: 'boop' }
+    }).then(function(response) {
+      assert.deepEqual(response.body, {foo: 'bar'});
+      done();
+    });
+  });
 });
